cba836d87da07b6936174749d94ce7244dee1fc7
Yi-Chun Chen <RimiChen@users.noreply.github.com>
2020-02-06 03:24:04 -0500

    upload filter table

--- Static/Layout.js
+++ Static/Layout.js
@@ -42,7 +42,37 @@ var labelList = [
     "LLL",
     "MMM"
 ];
-
+var numberList = [
+    1,
+    32,
+    24,
+    20,
+    80,
+    100,
+    50,
+    60,
+    70,
+    85,
+    98,
+    64,
+    15,
+    12,
+    57
+];
+var monthList = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC"
+];
 
 //function drawBlock(parent_frame, labelList, title, width, height, z, currentIndex, clickCallBack, callBackArgs)
 //normal_blocks
@@ -62,3 +92,19 @@ var labelList = [
 //function drawFoldBlock(parent_frame, labelList, barList, width, height, z, currentIndex, clickCallBack, callBackArgs)
 var out = VIS_E.drawFoldBlock("mid_main_frame", labelList, nameList, partWidth, partHeight, 2, element_index_count, graphdata.testFunction, []);
 
+//VIS_E.mapColorNumber(10, 0, 100, 32, [255, 0, 0]);
+//drawTableColmn(parent_frame, numberList, min, max, width, height, top, left, basicColor, label, clickCallBack, callBackArgs)
+var columnWidth = 80;
+var rowHeight = 30;
+var labelShift = 100;
+var topShift = 200;
+
+// draw selection table
+for(var i = 0; i < numberList.length; i++){
+    var number = Math.floor(30/numberList.length);
+    VIS_E.drawClickableTextLabel("mid_main_frame", i+65536, i*rowHeight+topShift, 0, 2, (i*number+1)+"~"+(i*number+number), 15, graphdata.testFunction, [])
+}
+
+for(var i = 0; i< monthList.length; i++){
+    VIS_E.drawTableColmn("mid_main_frame", numberList, 0, 100, columnWidth, rowHeight, topShift, i*columnWidth+labelShift, [12, 134, 0], monthList[i], graphdata.testFunction, []);
+}
--- Static/Vis_element.js
+++ Static/Vis_element.js
@@ -83,6 +83,53 @@ export function drawClickableTextLabel(parent_frame, index, top, left, z, text, 
     var parent = document.getElementById(parent_frame); ;
     parent.appendChild(canvas);
 }
+export function drawClickableTextColorLabel(parent_frame, index, width, height, top, left, z, text, textsize, color, clickCallBack, callBackArgs){
+    var canvas = document.createElement('canvas');
+
+    canvas.id = "canvasTextColor"+index;
+    canvas.width = width;
+    canvas.height =  height;
+    canvas.style.top = top;
+    //console.log(top)
+    canvas.style.left = left;    
+    canvas.style.zIndex = z;
+    canvas.style.position = "absolute";
+    canvas.style.border = "1px solid";
+
+    var ctx = canvas.getContext("2d");
+   
+    //var size = textsize +"px";
+    //ctx.fontsize = textsize;
+
+
+    ctx.font = "normal "+textsize+"px Arial";
+
+    ctx.fillStyle = color;
+    //console.log(color);
+    //console.log(typeof(color));
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    var txt = text;
+    ctx.fillText(txt, 10, 20);
+    //ctx.fillRect(left, top, canvas.width, canvas.height);
+    //console.log("w: "+ canvas.width+", h: "+canvas.height);
+
+    // Add event listener for `click` events.
+    var canvasLeft = canvas.offsetLeft;
+    var canvasTop = canvas.offsetTop;
+    //var context = canvas.getContext('2d');
+   
+    canvas.addEventListener('click', function(event) {
+        var x = event.pageX - canvasLeft,
+            y = event.pageY - canvasTop;
+
+            //console.log(canvas.id+": ("+x+", "+y+")");
+          clickCallBack.apply(this, callBackArgs);
+    }, false);    
+
+    var parent = document.getElementById(parent_frame); ;
+    parent.appendChild(canvas);
+}
 
 export function drawAxis(parent_frame, index, width, top, left, z, color, numberLabel, slotWidth){
 
@@ -275,8 +322,45 @@ export function drawFoldBlock(parent_frame, labelList, barList, width, height, z
     };
 }
 
-export function mapColorNumber(numberOfChunk, minNumber, maxNumber, basicColor){
+export function mapColorNumber(numberOfChunk, minNumber, maxNumber, number, basicColor){
     // we basicly change the alpha value
-    
+    var chunk = Math.floor((maxNumber - minNumber)/numberOfChunk);
+    var alphaValue = 1/numberOfChunk;
+    var color = basicColor;
+    // R G B
+    if(basicColor.length >=3){
+        var R = color[0];
+        var G = color[1];
+        var B = color[2];
+    }
+    else{
+        console.log("not correct color format");
+    }
+
+    var value = minNumber;
+    var count = 0;
+    while((value+chunk) <= number){
+        value = value+chunk;
+        //console.log(value);
+        count = count +1;
+    }
+
+    var computedColorAlpha = alphaValue * count;
+    var computedColor = "rgba("+R+", "+G+", "+B+", "+computedColorAlpha+")";
+    ///console.log(computedColor);
+
     return computedColor;
+}
+
+export function drawTableColmn(parent_frame, numberList, min, max, width, height, top, left, basicColor, label, clickCallBack, callBackArgs){
+    
+    var textsize = 15;
+    drawClickableTextLabel(parent_frame, 0, top - textsize-5, left, 2, label, textsize, clickCallBack, callBackArgs);
+    for(var i = 0; i < numberList.length; i++){
+        var min = min;
+        var max = max;
+        var color = mapColorNumber(10, min, max, numberList[i], basicColor);
+        drawClickableTextColorLabel(parent_frame, i, width, height, i*height+top, left, 2, numberList[i], 15, color, clickCallBack, callBackArgs);        
+    }
+
 }
